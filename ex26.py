fb8717a000eb7900fa915ad35b71ae9881eb5051
jessicahur4-gif <jessicarhur4@gmail.com>
2025-11-10 15:29:17 +0900

    과제 25~27번 완료

---
+++ ex26.py
@@ -0,0 +1,2 @@
+park = {'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83}
+print(park['english'], park['science'])
---
+++ ex27.py
@@ -0,0 +1,4 @@
+kim = {'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83}
+for subject in kim:
+    kim[subject] = 100
+print(kim)
